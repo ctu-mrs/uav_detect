dbe5da2699a4f20e41bbea65eda1d1a10fb2acad
Matous <vrbamato@fel.cvut.cz>
2018-07-29 19:08:05 +0200

    Added comments and some other minor changes

--- include/param_loader.h
+++ include/param_loader.h
@@ -42,11 +42,37 @@ template <typename ConfigType>
 class DynamicReconfigureMgr
 {
   public:
+    // this variable holds the latest received configuration
     ConfigType config;
+    // the callback itself
     void dynamic_reconfigure_callback(ConfigType& new_config, uint32_t level)
     {
       ROS_INFO("Dynamic reconfigure request received:");
       if (m_print_values)
+      {
+        print_changed_params(new_config);
+      }
+      m_not_initialized = false;
+      config = new_config;
+    };
+    // initialize some stuff in the constructor
+    DynamicReconfigureMgr(bool print_values = true)
+    {
+      m_print_values = print_values;
+      m_not_initialized = true;
+      // initialize the dynamic reconfigure callback
+      m_cbf = boost::bind(&DynamicReconfigureMgr<ConfigType>::dynamic_reconfigure_callback, this, _1, _2);
+      m_server.setCallback(m_cbf);
+    };
+
+  private:
+      bool m_print_values, m_not_initialized;
+      // dynamic_reconfigure server variables
+      typename dynamic_reconfigure::Server<ConfigType> m_server;
+      typename dynamic_reconfigure::Server<ConfigType>::CallbackType m_cbf;
+
+      // method for printing names and values of new received parameters (prints only the changed ones)
+      void print_changed_params(const ConfigType& new_config)
       {
         // Note that this part of the API is still unstable and may change! It was tested with ROS Kinetic
         std::vector<typename ConfigType::AbstractParamDescriptionConstPtr> descrs = new_config.__getParamDescriptions__();
@@ -83,23 +109,7 @@ class DynamicReconfigureMgr
           }
         }
       }
-      m_not_initialized = false;
-      config = new_config;
-    };
-    DynamicReconfigureMgr(bool print_values = true)
-    {
-      m_print_values = print_values;
-      m_not_initialized = true;
-      m_cbf = boost::bind(&DynamicReconfigureMgr<ConfigType>::dynamic_reconfigure_callback, this, _1, _2);
-      m_server.setCallback(m_cbf);
-    };
-
-  private:
-      bool m_print_values, m_not_initialized;
-      // dynamic_reconfigure server variables
-      typename dynamic_reconfigure::Server<ConfigType> m_server;
-      typename dynamic_reconfigure::Server<ConfigType>::CallbackType m_cbf;
-
+      // helper methods for automatic parameter value parsing
       template <typename T>
       inline bool try_cast(boost::any& val, T*& out)
       {
@@ -114,51 +124,11 @@ class DynamicReconfigureMgr
           /* std::cout << std::endl << *tmp << " vs " << *to_what << std::endl; */
           return *tmp == *to_what;
         } else
-        {
+        { // the value should not change during runtime - this should never happen (but its better to be safe than sorry)
           ROS_WARN("Value type has changed - this should not happen!");
           return false;
         }
       };
 };
-
-/* class LoadParam{ */
-/* private: */
-/*   ros::NodeHandle m_nh; */
-/*   std::string m_name; */
-/*   bool m_print_value; */
-/* public: */
-/*     LoadParam(ros::NodeHandle &nh, const std::string &name, bool print_value = true) */
-/*       : m_nh(nh), m_name(name) {}; */
-/*     /1* LoadParam(ros::NodeHandle &nh, const std::string &name, const T& default_value, bool print_value = true) *1/ */
-/*     /1*   : m_nh(nh), m_name(name), m_print_value(print_value), m_default_value(default_value), m_use_default(true) {}; *1/ */
-
-/*     template<typename T> */
-/*     operator T() */
-/*     { */
-/*       /1* if (m_use_default) *1/ */
-/*       /1* { *1/ */
-/*       /1*   T loaded; *1/ */
-/*       /1*   m_nh.param(m_name, &loaded, m_default_value); *1/ */
-/*       /1*   if (m_print_value) *1/ */
-/*       /1*     std::cout << "\t" << m_name << ":\t" << loaded << std::endl; *1/ */
-/*       /1*   return loaded; *1/ */
-/*       /1* } else *1/ */
-/*       { */
-/*         T loaded; */
-/*         bool success = m_nh.getParam(m_name, &loaded); */
-/*         if (success) */
-/*         { */
-/*           if (m_print_value) */
-/*             std::cout << "\t" << m_name << ":\t" << loaded << std::endl; */
-/*         } else */
-/*         { */
-/*           ROS_ERROR("Could not load non-optional parameter %s", m_name.c_str()); */
-/*           /1* load_successful = false; *1/ */
-/*         } */
-/*         return loaded; */
-/*       } */
-/*     }; */
-
-/* }; */
 
 #endif // PARAM_LOADER_H
