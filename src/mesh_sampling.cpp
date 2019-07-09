3ccfe09be766453976fa8d987a15798b03feed1c
Matouš Vrba <vrbamato@MyslPlacka>
2019-07-09 12:15:23 +0200

    mesh resampling now buildable

--- src/mesh_sampling.cpp
+++ src/mesh_sampling.cpp
@@ -1,5 +1,10 @@
 #include <mesh_sampling.h>
 
+using point_t = pcl::PointXYZ;
+using normal_t = pcl::Normal;
+using point_normal_t = pcl::PointNormal;
+using cloud_t = pcl::PointCloud<point_t>;
+
 struct triangle_t
 {
   Eigen::Vector3f A;
@@ -11,6 +16,14 @@ struct triangle_t
       const Eigen::Vector3f& C
       ) :
     A(A), B(B), C(C) {};
+  triangle_t(
+      const point_t& A,
+      const point_t& B,
+      const point_t& C
+      ) :
+    A(A.x, A.y, A.z),
+    B(B.x, B.y, B.z),
+    C(C.x, C.y, C.z) {};
 };
 
 double triangle_area(const triangle_t& tri)
@@ -23,7 +36,7 @@ double triangle_area(const triangle_t& tri)
   return area;
 }
 
-triangle_t get_triangle(const pcl::Vertices& poly, const pcl::PointCloud<pcl::PointXYZ>& mesh_cloud)
+triangle_t get_triangle(const pcl::Vertices& poly, const cloud_t& mesh_cloud)
 {
   assert(poly.vertices.size() == 3);
   const auto A = mesh_cloud.at(poly.vertices.at(0));
@@ -33,7 +46,7 @@ triangle_t get_triangle(const pcl::Vertices& poly, const pcl::PointCloud<pcl::Po
   return ret;
 }
 
-std::vector<float> cumulative_mesh_areas(const std::vector<pcl::Vertices>& mesh_polygons, const pcl::PointCloud<pcl::PointXYZ>& mesh_cloud)
+std::vector<float> cumulative_mesh_areas(const std::vector<pcl::Vertices>& mesh_polygons, const cloud_t& mesh_cloud)
 {
   float total_area = 0.0;
   std::vector<float> cumulative_areas;
@@ -55,11 +68,11 @@ float random(const float min, const float max)
   return dist(gen);
 }
 
-pcl::Point random_point_triangle(const triangle_t& tri)
+point_t random_point_triangle(const triangle_t& tri)
 {
   const float r1 = random(0.0, 1.0);
   const float r2 = random(0.0, 1.0);
-  const float r1sqr = std::sqrt(random_coord1);
+  const float r1sqr = std::sqrt(r1);
   const float omr1sqr = (1 - r1sqr);
   const float omr2 = (1 - r2);
   const float a1 = tri.A.x() * omr1sqr;
@@ -68,33 +81,63 @@ pcl::Point random_point_triangle(const triangle_t& tri)
   const float b1 = tri.B.x() * omr2;
   const float b2 = tri.B.y() * omr2;
   const float b3 = tri.B.z() * omr2;
-  const float c1  tri.C.x() * r1sqr * (r2 * c1 + b1) + a1;
-  const float c2  tri.C.y() * r1sqr * (r2 * c2 + b2) + a2;
-  const float c3  tri.C.z() * r1sqr * (r2 * c3 + b3) + a3;
-  pcl::Point ret(c1, c2, c3);
+  const float c1 = tri.C.x() * r1sqr * (r2 * tri.C.x() + b1) + a1;
+  const float c2 = tri.C.y() * r1sqr * (r2 * tri.C.y() + b2) + a2;
+  const float c3 = tri.C.z() * r1sqr * (r2 * tri.C.z() + b3) + a3;
+  point_t ret(c1, c2, c3);
   return ret;
 }
 
-pcl::PointNormal random_point_surface(const std::vector<pcl::Vertices>& mesh_polygons, const pcl::PointCloud<pcl::PointXYZ>& mesh_cloud, const std::vector<double>& cumulative_areas)
+normal_t triangle_normal(const triangle_t& tri)
+{
+  normal_t ret;
+  Eigen::Vector3f v1 = tri.A - tri.C;
+  Eigen::Vector3f v2 = tri.B - tri.C;
+  Eigen::Vector3f n = v1.cross(v2).normalized();
+  ret.normal_x = n.x();
+  ret.normal_y = n.y();
+  ret.normal_z = n.z();
+  return ret;
+}
+
+point_normal_t merge_point_normal(const point_t& pt, const normal_t& normal)
+{
+  point_normal_t ret;
+  ret.x = pt.x;
+  ret.y = pt.y;
+  ret.z = pt.z;
+  ret.normal_x = normal.normal_x;
+  ret.normal_y = normal.normal_y;
+  ret.normal_z = normal.normal_z;
+  return ret;
+}
+
+point_normal_t random_point_surface(const std::vector<pcl::Vertices>& mesh_polygons, const cloud_t& mesh_cloud, const std::vector<float>& cumulative_areas)
 {
   const float total_area = cumulative_areas.back();
   const float random_area = random(0.0, total_area);
   const auto& random_triangle_idx = std::lower_bound(std::begin(cumulative_areas), std::end(cumulative_areas), random_area) - std::begin(cumulative_areas);
   const pcl::Vertices random_triangle_poly = mesh_polygons.at(random_triangle_idx);
   const triangle_t& random_triangle = get_triangle(random_triangle_poly, mesh_cloud);
+  const point_t random_point = random_point_triangle(random_triangle);
+  const normal_t normal = triangle_normal(random_triangle);
+  const point_normal_t ret = merge_point_normal(random_point, normal);
+  return ret;
 }
 
-void uniform_mesh_sampling(const pcl::PolygonMesh& mesh, const size_t n_pts, pcl::PointCloud<pcl::PointNormal>& cloud_out)
+pcl::PointCloud<pcl::PointNormal> uniform_mesh_sampling(const pcl::PolygonMesh& mesh, const size_t n_pts)
 {
-  cloud_out.clear();
-  cloud_out.reserve(n_pts);
-  pcl::PointCloud<pcl::PointXYZ>::ConstPtr mesh_cloud;
+  pcl::PointCloud<pcl::PointNormal> ret;
+  ret.reserve(n_pts);
+  /* cloud_t::ConstPtr mesh_cloud = boost::make_shared<cloud_t>(mesh.cloud); */
+  cloud_t mesh_cloud;
   pcl::fromPCLPointCloud2(mesh.cloud, mesh_cloud);
   std::vector<float> cumulative_areas = cumulative_mesh_areas(mesh.polygons, mesh_cloud);
   for (size_t it = 0; it < n_pts; it++)
   {
-    pcl::PointNormal pt = random_point_surface(mesh, cumulative_areas);
-    cloud_out.push_back(pt);
+    pcl::PointNormal pt = random_point_surface(mesh.polygons, mesh_cloud, cumulative_areas);
+    ret.push_back(pt);
   }
+  return ret;
 }
 
